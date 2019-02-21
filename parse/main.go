5c14182aba4613d470b229352dae0c1e453ce767
Till Kahlbrock <till.kahlbrock@superluminar.io>
2019-02-21 07:42:47 +0100

    deduplicate items by using a set-like structure

--- parse/main.go
+++ parse/main.go
@@ -11,6 +11,12 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type Item struct {
+	bibNum  string
+	title   string
+	details string
+}
+
 func Handler() {
 	dat, err := ioutil.ReadFile("./sample.html")
 	if err != nil {
@@ -21,13 +27,32 @@ func Handler() {
 		log.Fatal(err)
 	}
 
+	items := map[string]Item{}
+
 	doc.Find("td.title").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the band and title
-		title := strings.Trim(s.Find("a.title").Text(), " ")
-		details := strings.Trim(s.Find("span.item-details").Text(), " ")
-		fmt.Printf("Item %d: %s - %s\n", i, title, details)
+		anchor := s.Find("a.title")
+		href, found := anchor.Attr("href")
+		if !found {
+			log.Fatal("No href attribute found")
+		}
+		parts := strings.Split(href, "=")
+		if len(parts) != 2 {
+			log.Fatalf("Could not split '%s' by '='", href)
+		}
+		id := parts[1]
+		if _, found := items[id]; !found {
+			items[id] = Item{
+				bibNum:  id,
+				title:   strings.Trim(anchor.Text(), " "),
+				details: strings.Trim(s.Find("span.item-details").Text(), " "),
+			}
+		}
 	})
-	fmt.Printf("got scheduled")
+
+	for _, i := range items {
+		fmt.Printf("%v\n", i)
+	}
 }
 
 func main() {
