10bbdb59ff5299e9382ec84d6d100fdd0f3995b6
Till Kahlbrock <till.kahlbrock@superluminar.io>
2019-02-21 07:18:39 +0100

    trim whitespaces

--- parse/main.go
+++ parse/main.go
@@ -23,8 +23,8 @@ func Handler() {
 
 	doc.Find("td.title").Each(func(i int, s *goquery.Selection) {
 		// For each item found, get the band and title
-		title := strings.Trim(s.Find("a.title").Text(), "\n")
-		details := strings.Trim(s.Find("span.item-details").Text(), "\n")
+		title := strings.Trim(s.Find("a.title").Text(), " ")
+		details := strings.Trim(s.Find("span.item-details").Text(), " ")
 		fmt.Printf("Item %d: %s - %s\n", i, title, details)
 	})
 	fmt.Printf("got scheduled")
