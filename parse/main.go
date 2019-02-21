67ea6bdc43880475be52495a84eb7d2979e0981d
Till Kahlbrock <till.kahlbrock@superluminar.io>
2019-02-21 07:17:21 +0100

    use goquery

--- parse/main.go
+++ parse/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func Handler() {
+	dat, err := ioutil.ReadFile("./sample.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(dat))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("td.title").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		title := strings.Trim(s.Find("a.title").Text(), "\n")
+		details := strings.Trim(s.Find("span.item-details").Text(), "\n")
+		fmt.Printf("Item %d: %s - %s\n", i, title, details)
+	})
 	fmt.Printf("got scheduled")
 }
 
