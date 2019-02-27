bd5ba47013e942627a68f3611977a7a188ba446b
Till Kahlbrock <till.kahlbrock@superluminar.io>
2019-02-27 17:08:39 +0100

    refactor and fancy logging

--- parse/main.go
+++ parse/main.go
@@ -4,11 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/aws/aws-lambda-go/lambda"
+	log "github.com/sirupsen/logrus"
 )
 
 type Item struct {
@@ -18,30 +18,40 @@ type Item struct {
 }
 
 func Handler() {
-	dat, err := ioutil.ReadFile("./sample.html")
+	dat := mustReadWebsiteData()
+	items, err := parseItemsFromHtml(dat)
 	if err != nil {
-		log.Fatal(err)
-	}
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(dat))
-	if err != nil {
-		log.Fatal(err)
+		log.WithError(err).Error("Failed to parse html")
 	}
 
+	for _, i := range items {
+		fmt.Printf("%v\n", i)
+	}
+}
+
+func parseItemsFromHtml(htmlData []byte) (map[string]Item, error) {
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(htmlData))
+	if err != nil {
+		log.WithError(err).Error("failed to read html data")
+		return nil, err
+	}
 	items := map[string]Item{}
-
 	doc.Find("td.title").Each(func(i int, s *goquery.Selection) {
-		// For each item found, get the band and title
 		anchor := s.Find("a.title")
 		href, found := anchor.Attr("href")
 		if !found {
-			log.Fatal("No href attribute found")
+			log.WithError(err).Errorf("No href attribute found in '%s'", anchor.Text())
+			return
 		}
 		parts := strings.Split(href, "=")
 		if len(parts) != 2 {
-			log.Fatalf("Could not split '%s' by '='", href)
+			log.Errorf("Could not split '%s' by '='", href)
+			return
 		}
 		id := parts[1]
 		if _, found := items[id]; !found {
+			err := fmt.Errorf("some error")
+			log.WithError(err).Infof("Found new item with id: '%s'", id)
 			items[id] = Item{
 				bibNum:  id,
 				title:   strings.Trim(anchor.Text(), " "),
@@ -49,10 +59,15 @@ func Handler() {
 			}
 		}
 	})
+	return items, nil
+}
 
-	for _, i := range items {
-		fmt.Printf("%v\n", i)
+func mustReadWebsiteData() []byte {
+	dat, err := ioutil.ReadFile("./sample.html")
+	if err != nil {
+		log.Fatal(err)
 	}
+	return dat
 }
 
 func main() {
